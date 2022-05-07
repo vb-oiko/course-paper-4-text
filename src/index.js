5f44afade96e677994ead4c90e8f80f12bd24fd5
Vasyl Boyko <vboiko0@gmail.com>
2022-05-07 13:04:50 +0300

    add paged.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import intro from "./source/intro.md";
 import chapter1 from "./source/chapter1.md";
 
+import "./style.css";
+
 const app = document.getElementById("app");
 
 const sections = [intro, chapter1];
@@ -10,3 +12,12 @@ sections.forEach((section) => {
   el.innerHTML = section;
   el.childNodes[0].childNodes.forEach((node) => app.appendChild(node));
 });
+
+import { Previewer } from "pagedjs";
+
+let paged = new Previewer();
+let flow = paged
+  .preview(document.querySelector("#app").innerHTML, ["/src/style.css"])
+  .then((flow) => {
+    console.log("Rendered", flow.total, "pages.");
+  });
